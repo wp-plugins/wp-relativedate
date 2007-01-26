c90e9d280171d3eec777e485c612f6c8f41a7f44
GamerZ <GamerZ@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-01-26 07:10:34 +0000

    Minor Fixes
    git-svn-id: https://plugins.svn.wordpress.org/wp-relativedate/trunk@7608 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- relativedate/relativedate.php
+++ relativedate/relativedate.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP-RelativeDate
 Plugin URI: http://www.lesterchan.net/portfolio/programming.php
-Description: Displays Relative Date To Your Post
+Description: Displays relative date alongside with your post/comments actual date. Like 'Today', 'Yesterday', '2 Days Ago', '2 Weeks Ago', '2 'Seconds Ago', '2 Minutes Ago', '2 Hours Ago'.
 Version: 1.10
 Author: GaMerZ
 Author URI: http://www.lesterchan.net
